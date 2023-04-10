d1d97b82b761bb9a3a17e298a8b77d1d1a266a6a
Maulvi Alfansuri <alfakatsuki@gmail.com>
2023-04-10 10:08:14 +0700

    Update force-exit.js

--- force-exit.js
+++ force-exit.js
@@ -1,3 +1,5 @@
+// Linux
+
 function findLibc() {
     const modules = Process.enumerateModulesSync();
     for (const module of modules) {
@@ -16,3 +18,27 @@ if (!libc) {
 
 const exit = new NativeFunction(libc.findExportByName('exit'), 'void', ['int']);
 exit(0);
+
+
+// Windows
+
+function findKernel32() {
+  const modules = Process.enumerateModulesSync();
+  for (const module of modules) {
+    if (module.name.toLowerCase() === 'kernel32.dll') {
+      return module;
+    }
+  }
+  return null;
+}
+
+const kernel32 = findKernel32();
+if (!kernel32) {
+  console.error('kernel32.dll not found in the process.');
+  return;
+}
+
+const ExitProcess = new NativeFunction(kernel32.findExportByName('ExitProcess'), 'void', ['uint32']);
+
+// Call ExitProcess function with an exit code of 0
+ExitProcess(0);
