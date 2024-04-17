3cd2884c875c46baabfb017d73ef4410e68ca82b
maulvialf <maulvialf@gmail.com>
2024-04-17 13:27:32 +0700

    add gitignore

--- attach-detach.js
+++
@@ -1,6 +0,0 @@
-function detachAfterRead() {
-    console.log(Process.memory.readByteArray(0x123456, 10)); // Read 10 bytes from memory address
-    process.detach();
-}
-  
-Java.perform(detachAfterRead);
