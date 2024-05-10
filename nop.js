753ce991a3fef3c953524334f15144935b1ac526
maulvialf <maulvialf@gmail.com>
2024-05-10 10:11:27 +0700

    add nop bytecode and boiler plate file

---
+++ nop.js
@@ -0,0 +1,9 @@
+function nop(addr) {
+    Memory.patchCode(ptr(addr), 4, code => {
+        const cw = new ThumbWriter(code, { pc: ptr(addr) });
+        cw.putNop();
+        cw.putNop();
+        cw.flush();
+    });
+}
+ 
