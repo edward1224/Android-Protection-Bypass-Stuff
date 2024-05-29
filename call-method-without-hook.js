c44b0d0bfd06509cacf813de7c954041c7b37f0a
Maulvi Alfansuri <alfakatsuki@gmail.com>
2024-05-30 00:24:17 +0700

    Update call-method-without-hook.js

--- call-method-without-hook.js
+++ call-method-without-hook.js
@@ -7,5 +7,20 @@ setTimeout(function() {
         var result = methodToCall.call(c, 'This is string');
         // Log the result
         console.log("Result of metod(String str): " + result);
+
+
+        // use below if did not     // Replace 'com.example.MyClass' with the actual class name that contains the native method
+        var MyClass = Java.use('com.example.MyClass');
+        
+        // Create an instance of the class if necessary
+        var instance = MyClass.$new();
+        
+        // Call the native method 'stringFromJNI'
+        var result = instance.stringFromJNI();
+        
+        // Print the result
+        console.log('Result from stringFromJNI: ' + result);work
+
+        
     });
 }, 0);
